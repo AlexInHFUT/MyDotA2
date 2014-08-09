a631dc834d67eb11802be2a1e32229807088642e
Alex Lai <rongchanglai@163.com>
2014-08-09 10:15:06 +0800

    go on working

--- src/com/alex/data/DotADB.java
+++ src/com/alex/data/DotADB.java
@@ -3,12 +3,20 @@ package com.alex.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import org.dom4j.*;
 
-public class DotADB extends SQLiteOpenHelper {
+import com.alex.dota2.MainActivity;
 
-	public DotADB(Context context, String dbName, int version){
+public class DotADB extends SQLiteOpenHelper implements Runnable{
+
+	private MainActivity activity;
+	private String account_id;
+
+
+	public DotADB(Context context, String dbName, int version,MainActivity activity){
 		super(context, dbName, null, version);
+		this.activity = activity;
 	}
 	
 	@Override
@@ -23,8 +31,21 @@ public class DotADB extends SQLiteOpenHelper {
 
 	}
 	
-	public void initData(String account_id){
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
+	}
+	
+	
+	
+	public String getAccount_id() {
+		return account_id;
+	}
+
+	public void setAccount_id(String account_id) {
+		this.account_id = account_id;
 	}
 
 }
